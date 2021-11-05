e60ad394858548e43e3365b7bcfad54fd2e53634
Ferdinando Letizia <ferdinando.letizia@outlook.it>
2021-11-05 15:13:42 +0100

    script

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const km = parseInt(prompt('Quanti Km si vuole percorrere?'));
+console.log(km);
+
+const anni = parseInt(prompt('Quanti anni ha il viaggitore?'));
+console.log(anni);
+
+let sconto;
+
+if (anni < 18) {
+    sconto = 20 / 100;
+} else if (anni >= 65) {
+    sconto = 40 / 100;
+    console.log(prezzo);
+} else {
+    sconto = 1;
+}
+
+let prezzo = km * 0.21 * sconto;
+console.log(prezzo);
+
+if ((isNaN(anni))||(isNaN(km))) {
+    prezzo = 'Non hai inserito un numero corretto';
+} else {
+    prezzo = 'Il prezzo calcolato è di € ' + prezzo.toFixed(2);
+}
+
+document.getElementById('prezzo-biglietto').innerHTML = prezzo;
